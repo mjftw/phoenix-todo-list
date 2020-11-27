d12f9a775eee24bdb6fa2bb5cd7e2bbc02da4f6b
Merlin Webster <mjftwebster@gmail.com>
2020-11-27 10:53:38 +0000

    Todo list now uses real data

--- lib/app_web/views/item_view.ex
+++ lib/app_web/views/item_view.ex
@@ -1,3 +1,9 @@
 defmodule AppWeb.ItemView do
   use AppWeb, :view
+
+  def complete(%{status: 1}), do: "completed"
+  def complete(%{status: _}), do: ""
+
+  def checked(%{status: 1}), do: "checked"
+  def checked(%{status: _}), do: ""
 end
